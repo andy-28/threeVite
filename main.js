c64915e9bff2a601fb632b769fd4744c4d569f99
andy <a5706617@gmail.com>
2022-12-16 16:07:50 +0800

    commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three"
 import "./style.css"
+import gsap from "gsap"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
 // Scene
@@ -22,6 +23,7 @@ const sizes = {
 // Light
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(10, 10, 10)
+// light.intensity = 1.25
 scene.add(light)
 
 // Camera
@@ -65,4 +67,26 @@ loop()
 
 // Timeline magic
 const tl = gsap.timeline({ defaults: { duration: 1 } })
-tl.fromTo(mesh.scale)
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0}, { z: 1, x: 1, y: 1})
+tl.fromTo("nav", { y: "-100%"}, {y: "0%" })
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 })
+
+// Mouse Animation
+let mouseDown = false
+let rgb = []
+window.addEventListener("mousedown", () => (mouseDown = true))
+window.addEventListener("mouseup", () => (mouseDown = false))
+
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown){
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+    // animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+    
+    gsap.to(mesh.material.color, {r: newColor.r, g: newColor.g, b: newColor.b,})
+  }
+})
