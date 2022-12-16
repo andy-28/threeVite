906dd49116c5ee16b044988b9ad3ad465ae6792b
andy <a5706617@gmail.com>
2022-12-16 15:50:57 +0800

    commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import { Thumbs } from "swiper"
 import * as THREE from "three"
+import "./style.css"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
 // Scene
 const scene = new THREE.Scene()
@@ -12,18 +13,56 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// Size
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
 // Light
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(10, 10, 10)
 scene.add(light)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 20
 scene.add(camera)
 
 // Renderer
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGL1Renderer({ canvas });
-renderer.setSize(800, 600)
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(2)
 renderer.render(scene, camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
+
+// Resize
+window.addEventListener("resize", () => {
+  // Update Sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  // Update Camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+})
+
+const loop = () => {
+  controls.update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
+}
+
+loop()
+
+// Timeline magic
+const tl = gsap.timeline({ defaults: { duration: 1 } })
+tl.fromTo(mesh.scale)
